daad9ca5c875849d1076361109f9037b407a326f
Aidaren <78560731+Aidaren@users.noreply.github.com>
2022-10-07 18:13:13 +0800

    Update Easy-BezierCurve.lua

--- Easy-BezierCurve.lua
+++ Easy-BezierCurve.lua
@@ -1,14 +1,17 @@
 --[[
 Made By Aidaren / 究极挨打人
-Credit: 老胡家的拖鞋
+Credits:
+老胡家的拖鞋
+枫
 
-Version - 1.2.1
+Version - 1.3.0
 
 ContactMe:
 WeChat: AidarenADR
 Discord: Aidaren#5009
 --]]
 
+local RunService = game:GetService("RunService")
 local Debris = game:GetService("Debris")
 
 --<模块>--
@@ -23,13 +26,13 @@ function BezierCurve.Lerp(Start , End , Time)
 	if typeof(End) ~= "Vector3" then
 		End = End.Position
 	end
-	
+
 	return Start + (End - Start) * Time
-	
+
 end
 
 function BezierCurve.GetFrameByDistance(Start , End , Multiply)
-	
+
 	if typeof(Start) ~= "Vector3" then
 		Start = Start.Position
 	end
@@ -37,25 +40,25 @@ function BezierCurve.GetFrameByDistance(Start , End , Multiply)
 	if typeof(End) ~= "Vector3" then
 		End = End.Position
 	end
-	
+
 	if not Multiply then
 		Multiply = 1
 	else
 		Multiply = 1 + Multiply
 	end
-	
+
 	local Distance = (Start - End).Magnitude--旋转半径
 	local Frame = math.round(Distance * 2 / Multiply)
-	
+
 	return Frame
-	
+
 end
 
 function BezierCurve.GetMotionTime(Frame , FPS)
 	local Time = 0
-	
+
 	Time += task.wait(1 / FPS) * Frame
-	
+
 	return Time
 end
 
@@ -85,7 +88,7 @@ function BezierCurve.GetMiddlePosition(StartPosition , TargetPosition , Angle , 
 
 	RotateCFrame = RotateCFrame * CFrame.Angles(0,0,math.rad(Angle)) --根据角度旋转此向量
 	local Radius = HalfVector3.Magnitude * Offset--旋转半径
-	
+
 	local ResultPosition = MiddlePosition + RotateCFrame.UpVector * Radius --中间位置 + 旋转后的上朝向 * 半径
 
 	return ResultPosition
@@ -105,7 +108,7 @@ function BezierCurve.Get2MiddlePosition(StartPosition , TargetPosition , Angle1 
 	if not Angle1 then
 		Angle1 = 0
 	end
-	
+
 	if not Angle2 then
 		Angle2 = 0
 	end
@@ -115,29 +118,29 @@ function BezierCurve.Get2MiddlePosition(StartPosition , TargetPosition , Angle1 
 	else
 		Offset1 = 1 + Offset1
 	end
-	
+
 	if not Offset2 then
 		Offset2 = 1
 	else
 		Offset2 = 1 + Offset2
 	end
-	
+
 	local function GetResultPosition(StartPosition , TargetPosition , Angle , Offset , Length)
-		
+
 		local HalfVector3 = (StartPosition - TargetPosition) * Length --起始与目标之间三分之一长度的向量
 		local MiddlePosition = StartPosition - HalfVector3 --中间位置点
 		local RotateCFrame = CFrame.new(MiddlePosition,TargetPosition) --从起始与目标之间的中心点指向目标的向量,用此向量进行旋转
-		
+
 		RotateCFrame = RotateCFrame * CFrame.Angles(0,0,math.rad(Angle)) --根据角度旋转此向量
 		local Radius = HalfVector3.Magnitude * Offset--旋转半径
-		
+
 		local ResultPosition = MiddlePosition + RotateCFrame.UpVector * Radius --中间位置 + 旋转后的上朝向 * 半径
-		
+
 	end
-	
+
 	local ResultPosition1 = GetResultPosition(StartPosition , TargetPosition , Angle1 , Offset1 , 1/3)
 	local ResultPosition2 = GetResultPosition(StartPosition , TargetPosition , Angle2 , Offset2 , 1/3 * 2)
-	
+
 	return ResultPosition1 , ResultPosition2
 
 end
@@ -145,7 +148,7 @@ end
 ------------------------------|分割线|------------------------------
 
 function BezierCurve.LinearBezierCurves(Frame , FPS , Target , Position1 , Position2)
-	
+
 	if typeof(Position1) ~= "Vector3" then
 		Position1 = Position1.Position
 	end
@@ -154,20 +157,39 @@ function BezierCurve.LinearBezierCurves(Frame , FPS , Target , Position1 , Posit
 		Position2 = Position2.Position
 	end
 
-	for Index = 0 , Frame , 1 do
-		
-		local Time = Index / Frame
-		
-		Target.Position = BezierCurve.Lerp(Position1 , Position2 , Time)
-		
-		task.wait(1 / FPS)
-		
-	end
-	
+	local Index = 0
+	local DeltaTime = 1 / FPS
+	local TotalTime = 0
+
+	local Event 
+	Event = RunService.Heartbeat:Connect(function(DeltaTime)
+
+		TotalTime += DeltaTime
+
+		if TotalTime < Index * DeltaTime then
+			return
+		end
+
+		if TotalTime >= Index * DeltaTime then
+
+			Index += 1
+
+			local Time = Index / Frame
+
+			Target.Position = BezierCurve.Lerp(Position1 , Position2 , Time)
+
+		end
+
+		if Index >= Frame then
+			Event:Disconnect()
+		end
+
+	end)
+
 end
 
 function BezierCurve.QuadraticBezierCurves(Frame , FPS , Target , Position1 , Position2 , Position3)
-	
+
 	if typeof(Position1) ~= "Vector3" then
 		Position1 = Position1.Position
 	end
@@ -179,24 +201,43 @@ function BezierCurve.QuadraticBezierCurves(Frame , FPS , Target , Position1 , Po
 	if typeof(Position3) ~= "Vector3" then
 		Position3 = Position3.Position
 	end
-	
-	for Index = 0 , Frame , 1 do
-		
-		local Time = Index / Frame
-		
-		local Lerp1 = BezierCurve.Lerp(Position1 , Position2 , Time)
-		local Lerp2 = BezierCurve.Lerp(Position2 , Position3 , Time)
-		
-		Target.Position = BezierCurve.Lerp(Lerp1 , Lerp2 , Time)
-		
-		task.wait(1 / FPS)
-		
-	end
-	
+
+	local Index = 0
+	local DeltaTime = 1 / FPS
+	local TotalTime = 0
+
+	local Event 
+	Event = RunService.Heartbeat:Connect(function(DT)
+
+		TotalTime += DT
+
+		if TotalTime < Index * DeltaTime then
+			return
+		end
+
+		if TotalTime >= Index * DeltaTime then
+
+			Index += 1
+
+			local Time = Index / Frame
+
+			local Lerp1 = BezierCurve.Lerp(Position1 , Position2 , Time)
+			local Lerp2 = BezierCurve.Lerp(Position2 , Position3 , Time)
+
+			Target.Position = BezierCurve.Lerp(Lerp1 , Lerp2 , Time)
+
+		end
+
+		if Index >= Frame then
+			Event:Disconnect()
+		end
+
+	end)
+
 end
 
 function BezierCurve.CubicBezierCurves(Frame , FPS , Target , Position1:Vector3 , Position2 , Position3 , Position4)
-	
+
 	if typeof(Position1) ~= "Vector3" then
 		Position1 = Position1.Position
 	end
@@ -208,28 +249,47 @@ function BezierCurve.CubicBezierCurves(Frame , FPS , Target , Position1:Vector3 
 	if typeof(Position3) ~= "Vector3" then
 		Position3 = Position3.Position
 	end
-	
+
 	if typeof(Position4) ~= "Vector3" then
 		Position4 = Position4.Position
 	end
-	
-	for Index = 0 , Frame , 1 do
-		
-		local Time = Index / Frame
-		
-		local Lerp1 = BezierCurve.Lerp(Position1 , Position2 , Time)
-		local Lerp2 = BezierCurve.Lerp(Position2 , Position3 , Time)
-		local Lerp3 = BezierCurve.Lerp(Position3 , Position4 , Time)
-		
-		local InLerp1 = BezierCurve.Lerp(Lerp1 , Lerp2 , Time)
-		local InLerp2 = BezierCurve.Lerp(Lerp2 , Lerp3 , Time)
-		
-		Target.Position = BezierCurve.Lerp(InLerp1 , InLerp2 , Time)
-		
-		task.wait(1 / FPS)
-		
-	end
-	
+
+	local Index = 0
+	local DeltaTime = 1 / FPS
+	local TotalTime = 0
+
+	local Event 
+	Event = RunService.Heartbeat:Connect(function(DeltaTime)
+
+		TotalTime += DeltaTime
+
+		if TotalTime < Index * DeltaTime then
+			return
+		end
+
+		if TotalTime >= Index * DeltaTime then
+
+			Index += 1
+
+			local Time = Index / Frame
+
+			local Lerp1 = BezierCurve.Lerp(Position1 , Position2 , Time)
+			local Lerp2 = BezierCurve.Lerp(Position2 , Position3 , Time)
+			local Lerp3 = BezierCurve.Lerp(Position3 , Position4 , Time)
+
+			local InLerp1 = BezierCurve.Lerp(Lerp1 , Lerp2 , Time)
+			local InLerp2 = BezierCurve.Lerp(Lerp2 , Lerp3 , Time)
+
+			Target.Position = BezierCurve.Lerp(InLerp1 , InLerp2 , Time)
+
+		end
+
+		if Index >= Frame then
+			Event:Disconnect()
+		end
+
+	end)
+
 end
 
 ------------------------------|分割线|------------------------------
@@ -246,32 +306,45 @@ function BezierCurve.LinearBezierCurvesLookAt(Frame , FPS , Target , Position1 ,
 	if typeof(Position1) ~= "Vector3" then
 		Position1 = Position1.Position
 	end
-	
+
 	if typeof(Position2) ~= "Vector3" then
 		Position2 = Position2.Position
 	end
-	
-	for Index = 0 , Frame , 1 do
-		
-		local Time = Index / Frame
-		
-		CFramePart.Position = BezierCurve.Lerp(Position1 , Position2 , Time)
-		
-		if Index == 0 then
-			continue
-		else
-			
+
+	local Index = 0
+	local DeltaTime = 1 / FPS
+	local TotalTime = 0
+
+	local Event
+	Event = RunService.Heartbeat:Connect(function(DeltaTime)
+
+		TotalTime += DeltaTime
+
+		if TotalTime < Index * DeltaTime then
+			return
+		end
+
+		if TotalTime >= Index * DeltaTime then
+
+			Index += 1
+
+			local Time = Index / Frame
+
+			CFramePart.Position = BezierCurve.Lerp(Position1 , Position2 , Time)
+
 			Target.CFrame = CFrame.lookAt(Target.Position , CFramePart.Position , Vector3.new(0,1,0)) * CFrameOffset
 			Target.Position = CFramePart.Position
-			
+
 		end
-		
-		task.wait(1 / FPS)
-		
-	end
-	
+
+		if Index >= Frame then
+			Event:Disconnect()
+		end
+
+	end)
+
 	Debris:AddItem(CFramePart , task.wait())
-	
+
 end
 
 function BezierCurve.QuadraticBezierCurvesLookAt(Frame , FPS , Target , Position1 , Position2 , Position3 , CFrameOffset)
@@ -296,30 +369,43 @@ function BezierCurve.QuadraticBezierCurvesLookAt(Frame , FPS , Target , Position
 		Position3 = Position3.Position
 	end
 
-	for Index = 0 , Frame , 1 do
+	local Index = 0
+	local DeltaTime = 1 / FPS
+	local TotalTime = 0
 
-		local Time = Index / Frame
+	local Event
+	Event = RunService.Heartbeat:Connect(function(DeltaTime)
 
-		local Lerp1 = BezierCurve.Lerp(Position1 , Position2 , Time)
-		local Lerp2 = BezierCurve.Lerp(Position2 , Position3 , Time)
+		TotalTime += DeltaTime
 
-		CFramePart.Position = BezierCurve.Lerp(Lerp1 , Lerp2 , Time)
+		if TotalTime < Index * DeltaTime then
+			return
+		end
 
-		if Index == 0 then
-			continue
-		else
+		if TotalTime >= Index * DeltaTime then
+
+			Index += 1
+
+			local Time = Index / Frame
+
+			local Lerp1 = BezierCurve.Lerp(Position1 , Position2 , Time)
+			local Lerp2 = BezierCurve.Lerp(Position2 , Position3 , Time)
+
+			CFramePart.Position = BezierCurve.Lerp(Lerp1 , Lerp2 , Time)
 
 			Target.CFrame = CFrame.lookAt(Target.Position , CFramePart.Position , Vector3.new(0,1,0)) * CFrameOffset
 			Target.Position = CFramePart.Position
 
 		end
 
-		task.wait(1 / FPS)
+		if Index >= Frame then
+			Event:Disconnect()
+		end
 
-	end
-	
+	end)
+
 	Debris:AddItem(CFramePart , task.wait())
-	
+
 end
 
 function BezierCurve.CubicBezierCurvesLookAt(Frame , FPS , Target , Position1 , Position2 , Position3 , Position4 , CFrameOffset)
@@ -343,39 +429,53 @@ function BezierCurve.CubicBezierCurvesLookAt(Frame , FPS , Target , Position1 , 
 	if typeof(Position3) ~= "Vector3" then
 		Position3 = Position3.Position
 	end
-	
+
 	if typeof(Position4) ~= "Vector3" then
 		Position4 = Position4.Position
 	end
 
-	for Index = 0 , Frame , 1 do
-		
-		local Time = Index / Frame
-		
-		local Lerp1 = BezierCurve.Lerp(Position1 , Position2 , Time)
-		local Lerp2 = BezierCurve.Lerp(Position2 , Position3 , Time)
-		local Lerp3 = BezierCurve.Lerp(Position3 , Position4 , Time)
-		
-		local InLerp1 = BezierCurve.Lerp(Lerp1 , Lerp2 , Time)
-		local InLerp2 = BezierCurve.Lerp(Lerp2 , Lerp3 , Time)
-		
-		CFramePart.Position = BezierCurve.Lerp(InLerp1 , InLerp2 , Time)
-		
-		if Index == 0 then
-			continue
-		else
-			
+	local Index = 0
+	local DeltaTime = 1 / FPS
+	local TotalTime = 0
+
+	local Event
+	Event = RunService.Heartbeat:Connect(function(DeltaTime)
+
+		TotalTime += DeltaTime
+
+		if TotalTime < Index * DeltaTime then
+			return
+		end
+
+		if TotalTime >= Index * DeltaTime then
+
+			Index += 1
+
+			local Time = Index / Frame
+
+			local Lerp1 = BezierCurve.Lerp(Position1 , Position2 , Time)
+			local Lerp2 = BezierCurve.Lerp(Position2 , Position3 , Time)
+			local Lerp3 = BezierCurve.Lerp(Position3 , Position4 , Time)
+
+			local InLerp1 = BezierCurve.Lerp(Lerp1 , Lerp2 , Time)
+			local InLerp2 = BezierCurve.Lerp(Lerp2 , Lerp3 , Time)
+
+			CFramePart.Position = BezierCurve.Lerp(InLerp1 , InLerp2 , Time)
+
 			Target.CFrame = CFrame.lookAt(Target.Position , CFramePart.Position , Vector3.new(0,1,0)) * CFrameOffset
 			Target.Position = CFramePart.Position
 
 		end
 
-		task.wait(1 / FPS)
+		if Index >= Frame then
+			Event:Disconnect()
+		end
 
-	end
-	
+	end)
+
+
 	Debris:AddItem(CFramePart , task.wait())
-	
+
 end
 
 return BezierCurve
